7b5e2465c00f5691f5dd75196e8ef9dcbb0ec82c
Nitin <kadyan.nitin2002@gmail.com>
2021-10-13 14:44:17 +0530

    Added time for different countries

--- project1.js
+++ project1.js
@@ -1,6 +1,27 @@
 let a;
 let date;
 let time;
+let wrdTime;
+let NYtime;
+let LDtime;
+let TItime;
+let DAtime;
+let YMtime;
+
+function worldTime (hrs){
+    if(a.getHours() < hrs){
+        wrdTime = 24 - hrs + a.getHours();
+    }
+    else{
+        wrdTime = a.getHours() - hrs;
+    }
+    if(wrdTime > 24){
+        wrdTime = wrdTime - 24;
+    }
+    return wrdTime;
+}
+
+
 const options = {
   weekday: "long",
   year: "numeric",
@@ -12,4 +33,19 @@ setInterval(() => {
   date = a.toLocaleDateString(undefined, options);
   time = a.getHours() + ":" + a.getMinutes() + ":" + a.getSeconds();
   document.getElementById("time").innerHTML = time + "<br> on " + date;
+  
+  NYtime = worldTime(10) + ":" + a.getMinutes() + ":" + a.getSeconds();
+  document.getElementById("NYtime").innerHTML = NYtime;
+
+  LDtime = worldTime(5) + ":" + a.getMinutes() + ":" + a.getSeconds();
+  document.getElementById("LDtime").innerHTML = LDtime;
+
+  TItime = worldTime(2) + ":" + a.getMinutes() + ":" + a.getSeconds();
+  document.getElementById("TItime").innerHTML = TItime;
+
+  DAtime = worldTime(-4) + ":" + a.getMinutes() + ":" + a.getSeconds();
+  document.getElementById("DAtime").innerHTML = DAtime;
+
+  YMtime = worldTime(-1) + ":" + a.getMinutes() + ":" + a.getSeconds();
+  document.getElementById("YMtime").innerHTML = YMtime;
 }, 1000);
